e885bbcbf7802b17ccc5c3bafec4e613c0c9ebb0
matyasszanto <matyasszanto@gmail.com>
2022-03-29 16:55:51 +0200

    Refactor

--- DEMO/renamer.py
+++ DEMO/renamer.py
@@ -11,17 +11,27 @@ import definitions
 
 def main():
 
-    # get image filenames
+    # get project basepath
     proj_path = definitions.get_project_path()
-    base_path = f"{proj_path}/DEMO/Database/926_plates"
+
+    # set image folder of images to be renamed
+    base_path = f"{proj_path}/DEMO/Database/03_29_plates"
+
+    # set original csv file path
+    csv_path = f"{proj_path}/rendszamok_regular_unique_03_29.csv"
+
+    # get image filenames
     filenames = sorted(os.listdir(path=base_path))
 
-    with open(file=f"{proj_path}/rendszamok_regular_unique_926.csv", newline="") as csvfile:
+    # create dictionary from csv file as {image_name: license_plate}
+    with open(file=csv_path, newline="") as csvfile:
 
+        # load and separate csv file into columns
         num_plates_csv = csv.reader(csvfile,
-                                    delimiter=";",
+                                    delimiter='\t',
                                     )
 
+        # create arrays of image_names and license_plates
         img_names = []
         license_plates = []
         for i, row in enumerate(num_plates_csv):
@@ -30,23 +40,25 @@ def main():
             if i == 0:
                 continue
 
+            # image name
             link = row[5]
             img_names.append(link[-12:-4])
-            print(img_names)
 
+            # license plate
             license_plate = row[1]
             license_plates.append(license_plate)
 
+        # create dictionary
         plates_dict = dict(zip(img_names, license_plates))
-        print(plates_dict)
 
     # iterate through filenames
     for file in filenames:
 
-        # find file in dictionary and rename file
+        # find file in dictionary and rename it
         file_key = file[:8]
         license_plate = plates_dict[file_key]
         os.rename(src=f"{base_path}/{file}", dst=f"{base_path}/{license_plate}.png")
+
     return
 
 
